22a1d8372c24337e509967bbb2bca38d9d6e11a1
Jimz2245 <144721957+Jimz2245@users.noreply.github.com>
2023-09-15 13:58:32 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main 
 {
   public static void main(String[] args) 
@@ -9,6 +10,23 @@ public class Main
    sunny = true;
    System.out.println(age);
    System.out.println(number);
-   System.out.print(sunny);
+   System.out.println(sunny);
+   
+   System.out.println("Welcome to cup pong, please input your username!");
+   System.out.println();
+   Scanner sc = new Scanner(System.in);
+   String name = sc.nextLine();
+   System.out.println("Hello "+name+ " please throw your ball.");
+   int cups = 10;
+   int landed = 0;
+   int missed = 0;
+   System.out.println("You landed it!");
+   cups--;
+   landed++;
+   System.out.println("So far you have landed "+landed+" shots, and missed "+missed+" shots!");
+   System.out.println(cups+" cups remain");
+   double percent = landed/(landed + missed);
+   System.out.println("that means you have landed "+ 100*percent+" percent of your shots!"); 
+     
   }
 }
