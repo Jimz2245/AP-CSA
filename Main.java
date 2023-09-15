1589fd61f5ba6e762c473638faee1d899404f487
Jimz2245 <144721957+Jimz2245@users.noreply.github.com>
2023-09-15 14:04:03 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,14 +3,14 @@ public class Main
 {
   public static void main(String[] args) 
   {
-   final int age = 17; //final means this value can't be changed, int means this is an integer value
-   double number; //double means that this is a decimal value
-   number = 2; 
-   boolean sunny; //boolean means this is a true or false scenario
-   sunny = true;
-   System.out.println(age);
-   System.out.println(number);
-   System.out.println(sunny);
+   final int age = 18; //final means this value can't be changed, int means this is an integer value
+   double fee; //double means that this is a decimal value
+   fee = 2.50; 
+   boolean open; //boolean means this is a true or false scenario
+   open = true;
+   System.out.println("you are "+age+" years old!");
+   System.out.println("the fee is "+fee+" dollars");
+   System.out.println("it is"+open+" that the table is open");
    
    System.out.println("Welcome to cup pong, please input your username!");
    System.out.println();
@@ -27,6 +27,6 @@ public class Main
    System.out.println(cups+" cups remain");
    double percent = landed/(landed + missed);
    System.out.println("that means you have landed "+ 100*percent+" percent of your shots!"); 
-     
+
   }
 }
