077dfe292aaf16aeb0dc167b79e40d5c1ca10fef
Jimz2245 <144721957+Jimz2245@users.noreply.github.com>
2023-09-21 13:52:29 -0400

    Add files via upload

---
+++ lamp.java
@@ -0,0 +1,19 @@
+public class lamp
+{
+  private String color;
+  private Boolean lightBulb;
+  public void lightUp()
+  {
+    System.out.print("It is now bright");
+  }
+  public void electricity()
+  {
+    System.out.print("now using your hard earned money on electricity");
+  }
+  public void ballJoint()
+  {
+    System.out.print("turns left");
+  }
+}
+  
+  
